b75deb43bea4af900137621d61f5aecd6f15c92b
lies93 <hocini.lies@gmail.com>
2022-02-22 14:54:54 +0100

    Exercice 3 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Steps } from "./components/Steps";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax =100
+const stepsMin = 0
+const stepsMax = 50000
+
+ export default function App() {
+    return (
+      <div class='container-fluid'>
+          <div class='row'>
+              <Steps></Steps>
+              <p> heart : {heartMin}</p>
+              <p> temperature : {tempMin}</p>
+              <p> steps : {stepsMin}</p>
+          </div>
+       
+      </div>
+    );
 }
-
-export default App;
---
+++ src/components/Steps.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+ export function Steps() {
+    return (
+      <div className="box col-sm-3 col-6">
+          <span className="material-icons directions_walk" >directions_walk</span>
+
+      </div>
+         
+    )
+}
